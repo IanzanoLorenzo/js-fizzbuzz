9c44b7efc8757dbe5c37432ccb899e4c2365195c
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-19 15:27:57 +0200

    bonus 1

---
+++ BONUS 1/JS/script.js
@@ -0,0 +1,20 @@
+let risposta;
+
+for (let n = 1; n <= 100; n++){
+        
+    let resto3  = n % 3;
+    let resto5  = n % 5;
+    risposta = ''
+
+    if( resto3 === 0){
+        risposta += 'Frizz'
+    }
+    if( resto5 === 0){
+        risposta += 'Buzz'
+    }
+    if( resto3 !== 0 && resto5 !== 0 ){
+        risposta = n
+    }
+
+    console.log(risposta)
+}
