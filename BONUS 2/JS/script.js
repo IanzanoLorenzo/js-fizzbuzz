f3d885e5b74db0a0799a73163569dce2de6f684f
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-19 17:17:26 +0200

    bonus 2 done

--- BONUS 2/JS/script.js
+++ BONUS 2/JS/script.js
@@ -1,24 +1,37 @@
 "use strict";
 
-let risposta;
-const lista = document.getElementById('lista')
+let risposta, stile;
+const lista = document.getElementById('lista');
 
 for (let n = 1; n <= 100; n++){
         
     let resto3  = n % 3;
     let resto5  = n % 5;
-    risposta = ''
+    risposta = '';
 
     if( resto3 === 0){
-        risposta += 'Frizz'
+        risposta += 'Frizz';
     }
     if( resto5 === 0){
-        risposta += 'Buzz'
+        risposta += 'Buzz';
     }
     if( resto3 !== 0 && resto5 !== 0 ){
-        risposta = n
+        risposta = n;
     }
 
-    lista.innerHTML +=  `<li>${risposta}</li>`
+    const listItem = document.createElement('li');
+    listItem.innerText = risposta;
+
+    if( resto3 === 0 ){
+        listItem.className = 'color1';
+    }
+    if( resto5 === 0){
+        listItem.className = 'color2';
+    }
+    if( resto3 === 0 && resto5 === 0 ){
+        listItem.className = 'color3';
+    }
+
+    lista.append(listItem);
 
 }
