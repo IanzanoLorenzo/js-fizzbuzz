cc7319ad69aface076e7ed94c204e244e2a62c3e
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-20 12:00:30 +0200

    Bonus 2 Bonus personale

--- BONUS 2/JS/script.js
+++ BONUS 2/JS/script.js
@@ -5,6 +5,7 @@ const lista = document.getElementById('lista');
 let primoNumero = document.getElementById('primoNumero');
 let secondoNumero = document.getElementById('secondoNumero');
 
+
 document.addEventListener('keyup', function (){
     lista.innerHTML = ''
     primaScelta = primoNumero.value;
@@ -44,3 +45,36 @@ document.addEventListener('keyup', function (){
     }
 }
 )
+
+for (let n = 1; n <= 100; n++){
+        
+    let primoResto  = n % primaScelta;
+    let secondoResto  = n % secondaScelta;
+    risposta = '';
+
+    if( primoResto === 0){
+        risposta += 'Fizz';
+    }
+    if( secondoResto === 0){
+        risposta += 'Buzz';
+    }
+    if( risposta === '' ){
+        risposta = n;
+    }
+
+    const listItem = document.createElement('li');
+    listItem.innerText = risposta;
+
+    if( primoResto === 0 ){
+        listItem.className += 'color1';
+    }
+    if( secondoResto === 0){
+        listItem.className += 'color2';
+    }
+    if( listItem.className === 'color1color2' ){
+        listItem.className = 'color3';
+    }
+
+    lista.append(listItem);
+
+}
