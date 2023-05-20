b49264666e188aa465049a39d3db61bf576d36f5
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-20 11:56:55 +0200

    Bonus 2 bonus personale

--- BONUS 2/JS/script.js
+++ BONUS 2/JS/script.js
@@ -1,37 +1,46 @@
 "use strict";
 
-let risposta, stile;
+let risposta, stile, primaScelta,secondaScelta;
 const lista = document.getElementById('lista');
+let primoNumero = document.getElementById('primoNumero');
+let secondoNumero = document.getElementById('secondoNumero');
 
-for (let n = 1; n <= 100; n++){
+document.addEventListener('keyup', function (){
+    lista.innerHTML = ''
+    primaScelta = primoNumero.value;
+    secondaScelta = secondoNumero.value;
+
+    for (let n = 1; n <= 100; n++){
         
-    let primoResto  = n % 3;
-    let secondoResto  = n % 5;
-    risposta = '';
-
-    if( primoResto === 0){
-        risposta += 'Fizz';
+        let primoResto  = n % primaScelta;
+        let secondoResto  = n % secondaScelta;
+        risposta = '';
+    
+        if( primoResto === 0){
+            risposta += 'Fizz';
+        }
+        if( secondoResto === 0){
+            risposta += 'Buzz';
+        }
+        if( risposta === '' ){
+            risposta = n;
+        }
+    
+        const listItem = document.createElement('li');
+        listItem.innerText = risposta;
+    
+        if( primoResto === 0 ){
+            listItem.className += 'color1';
+        }
+        if( secondoResto === 0){
+            listItem.className += 'color2';
+        }
+        if( listItem.className === 'color1color2' ){
+            listItem.className = 'color3';
+        }
+    
+        lista.append(listItem);
+    
     }
-    if( secondoResto === 0){
-        risposta += 'Buzz';
-    }
-    if( risposta === '' ){
-        risposta = n;
-    }
-
-    const listItem = document.createElement('li');
-    listItem.innerText = risposta;
-
-    if( primoResto === 0 ){
-        listItem.className += 'color1';
-    }
-    if( secondoResto === 0){
-        listItem.className += 'color2';
-    }
-    if( listItem.className === 'color1color2' ){
-        listItem.className = 'color3';
-    }
-
-    lista.append(listItem);
-
 }
+)
